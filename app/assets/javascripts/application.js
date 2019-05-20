8a45daba00bb7ba3bf5366c7611a17fdf71170b8
Ben Onah <benjamin.onah@andela.com>
2019-05-20 04:38:28 +0100

    Set up form and style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+  const form = $('#form');
+  form.submit(function(event) {
+    event.preventDefault();
+    console.log("Bandf");
+  })
+})
