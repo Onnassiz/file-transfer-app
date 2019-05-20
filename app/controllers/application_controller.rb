e417fae55788b56df427957ef87cd99d8e18bff4
Ben Onah <benjamin.onah@andela.com>
2019-05-20 19:16:22 +0100

    Implement file upload and download with Active Storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   def index
     render 
   end
-  
-  def upload
-    
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'application#index'
-  post '/', to: 'application#upload'
+  resources :documents
+  root 'documents#index'
 end
